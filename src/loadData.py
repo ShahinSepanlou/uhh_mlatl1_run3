fea18c0e4b798a48e6b6cecab643dc83f7ea65ca
Finn <flabe@physnet.uni-hamburg.de>
2022-11-01 16:56:09 +0100

    Add first version of anomaly team signal reading method

--- src/loadData.py
+++ src/loadData.py
@@ -23,15 +23,75 @@ vector.register_awkward()
 
 # I created two functions for h5s, as the signal one contains multiple signals that we (might) want to load individually
 
-def readFromAnomalySignalh5(inputfile):
-    # TODO: implement
+def readFromAnomalySignalh5(inputfile, process, moreInfo=None, verbosity = 0):
 
-    raise Exception("readFromAnomalySignalh5 is not implemented yet!")
+    if(verbosity > 0): print("Reading anomaly team preprocessed signal file at " + inputfile + " for process " + process + ".")
+    
+    # constructing the information dict
+    # some things will be automatically filled here
+    # the input "moreInfo" can be used to pass more information
+    # this information will have priority over automatically set entries
+    infoDict = {}
+    infoDict["input"] = inputfile
+    
+    # preparing lists to store L1 bit info
+    L1bits_labels = []
+    L1bits = []
+    
+    # reading the intput file
+    if(verbosity > 0): print("Starting to read input file...")
+    with h5py.File(inputfile, 'r') as h5f2:
+
+        for key in h5f2.keys():
+            
+            if key.startswith(process + "_L1_"):
+                L1bits_labels.append(key.replace(process+"_", ""))
+                L1bits.append(np.array(h5f2[key]))
+            elif key == process + "_l1bit":
+                L1bit = np.array(h5f2[key])
+
+            # doing this should remove all trigger things, and leave a single entry with the data
+            if len(h5f2[key].shape) < 3: continue
+            if key == process: data = h5f2[key][:,:,:]
+    
+    # splitting objects
+    np_energysums = data[:,0,:].reshape( (data.shape[0], 1, 3) ) # reshape is needed to keep dimensionality
+    np_egammas = data[:,1:4,:]
+    np_muons = data[:,5:8,:]
+    np_jets = data[:,9:19,:]
+    
+    # converting to awkward (thanks Artur for the code)
+    ak_egammas = ak.zip( {key:ak.from_regular(np_egammas[:,:,i], axis = 1) for i,key in enumerate(["pt","eta","phi"])}, with_name = "Momentum4D")
+    ak_muons = ak.zip( {key:ak.from_regular(np_muons[:,:,i], axis = 1) for i,key in enumerate(["pt","eta","phi"])}, with_name = "Momentum4D")
+    ak_jets = ak.zip( {key:ak.from_regular(np_jets[:,:,i], axis = 1) for i,key in enumerate(["pt","eta","phi"])}, with_name = "Momentum4D")
+    
+    # energy sums are handled a bit differently
+    ak_energysums = ak.zip( {key:ak.from_regular(np_energysums[:,:,2*i], axis = 1) for i, key in enumerate(["pt","phi"])}, with_name = "Momentum4D")
+    ak_energysums["Type"] = [2] * len(ak_energysums) # MET should have Type 2
+    
+    # removing empty entries (not needed for energy sums)
+    ak_egammas = ak_egammas[ak_egammas.pt > 0]
+    ak_muons = ak_muons[ak_muons.pt > 0]
+    ak_jets = ak_jets[ak_jets.pt > 0]
+    
+    infoDict["nEvents"] = len(ak_muons)
+    
+    # formating the L1 bits
+    df_total_L1 = pd.DataFrame( {"total L1": L1bit} )
+    df_trigger_bits = pd.DataFrame(np.asarray(L1bits).T, columns=L1bits_labels)
+    df_bits = df_total_L1.join(df_trigger_bits)
+    
+    # after everything else: add moreInfo
+    if moreInfo: infoDict = {**infoDict, **moreInfo}
+        
+    if(verbosity > 0): print("Done!")
+    
+    return infoDict, ak_muons, ak_egammas, ak_jets, ak_energysums, df_bits
 
 
 def readFromAnomalyBackgroundh5(inputfile, moreInfo=None, verbosity = 0):
     
-    if(verbosity > 0): print("Reading anomaly team preprocessed file at " + inputfile + ".")
+    if(verbosity > 0): print("Reading anomaly team preprocessed background file at " + inputfile + ".")
         
     # constructing the information dict
     # some things will be automatically filled here

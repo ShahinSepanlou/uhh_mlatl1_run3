39a43b5dbb5499c16054c4c870eb77be351d3474
Finn <flabe@physnet.uni-hamburg.de>
2022-11-02 16:37:18 +0100

    Add method to define trigger

--- src/inference.py
+++ src/inference.py
@@ -1,7 +1,7 @@
 import keras
+import pandas as pd
 
-# Function to run the inference on the dataset
-
+# Functions to run the inference on the dataset
 def runInference(model_file, x, verbosity = 0):
     # Expected input:
     # - path to the model file
@@ -18,3 +18,21 @@ def runInference(model_file, x, verbosity = 0):
     y_pred = model.predict(x)
     
     return y_pred
+
+
+# quick method to define a trigger from a certain threshold
+# this will return the "trigger bit" of this trigger as a dataframe
+# other ways to define a trigger from a y_pred can be defined
+def defineTriggerFromThreshold(y_pred, threshold, label = None, mode = "min", verbosity = 0):
+    
+    if label: label = label + "_"
+    else: label = ""
+    
+    if mode == "min":
+        if (verbosity > 0): print("Defining Trigger by requiring score > " + str(threshold) + ".")
+        return pd.DataFrame( y_pred > threshold, columns=[label + mode + "_" + str(threshold).replace(".", "p")] )
+    elif mode == "max":
+        if (verbosity > 0): print("Defining Trigger by requiring score < " + str(threshold) + ".")  
+        return pd.DataFrame( y_pred < threshold, columns=[label + mode + "_" + str(threshold).replace(".", "p")] )
+    else:
+        raise Exception("Mode " + mode + " not recognized.")
